75f6f73ef8e18bb76258d6dfcdf9a491e7e87ab7
Clyde Shaffer <clydeshaffer@gmail.com>
2021-07-01 15:32:39 -0400

    detect wall collisions

--- src/main.c
+++ src/main.c
@@ -38,29 +38,73 @@ void updateInputs(){
 #define TILE_ORD 5
 #define VISIBLE_W 5
 #define VISIBLE_H 5
+#define CAMERA_LIMIT 895 // (TILE_SIZE * (MAP_W - VISIBLE_W + 1)) - 1
 
-unsigned char camera_x = 0;
-unsigned char camera_y = 0;
-unsigned char tile_scroll_x = 0;
-unsigned char tile_scroll_y = 0;
+unsigned int camera_x = 0;
+unsigned int camera_y = 0;
 unsigned char tiles[MAP_SIZE];
 extern const unsigned char* TestMap;
 extern const SpriteMetadata* HeroMeta;
 unsigned char walk_cycle[4] = {3, 4, 5, 6};
+unsigned int player_x = 48, player_y = 48;
+
+#define HITBOX_X -6
+#define HITBOX_Y 4
+#define HITBOX_W 10
+#define HITBOX_H 3
+
+char tile_is_passable(char* tile) {
+    return *tile == 1 || *tile == 3;
+}
+
+char character_tilemap_check(unsigned int pos_x, unsigned int pos_y) {
+    char *corner_tile;
+    pos_x += HITBOX_X;
+    pos_y += HITBOX_Y;
+    corner_tile = tiles + (pos_x >> TILE_ORD) + ((pos_y >> TILE_ORD) << MAP_ORD);
+    pos_x &= (TILE_SIZE-1);
+    pos_y &= (TILE_SIZE-1);
+    if(!tile_is_passable(corner_tile)) {
+        return 0;
+    }
+    corner_tile++;
+    if(pos_x + HITBOX_W >= TILE_SIZE) {
+        if(!tile_is_passable(corner_tile)) {
+            return 0;
+        }   
+    }
+    corner_tile += MAP_W - 1;
+    if(pos_y + HITBOX_H >= TILE_SIZE) {
+        if(!tile_is_passable(corner_tile)) {
+            return 0;
+        }   
+    }
+    corner_tile++;
+    if((pos_x + HITBOX_W >= TILE_SIZE) && (pos_y + HITBOX_H >= TILE_SIZE)) {
+        if(!tile_is_passable(corner_tile)) {
+            return 0;
+        }   
+    }
+    return 1;
+}
 
 void draw_world() {
-    int r, c;
+    char r, c, tile_scroll_x, tile_scroll_y, cam_x, cam_y;
     int t;
+    tile_scroll_x = camera_x & (TILE_SIZE-1);
+    tile_scroll_y = camera_y & (TILE_SIZE-1);
+    cam_x = camera_x >> TILE_ORD;
+    cam_y = camera_y >> TILE_ORD;
 
     r = 0;
     c = 0;
-    t = (camera_x + c) + ((camera_y + r) << MAP_ORD);
+    t = (cam_x + c) + ((cam_y + r) << MAP_ORD);
     if(tiles[t] != 0) {
         QueueSpriteRect(0, 0, TILE_SIZE - tile_scroll_x, TILE_SIZE - tile_scroll_y, tile_scroll_x + (tiles[t] << TILE_ORD), tile_scroll_y, 0);
     }
     t++;
     for(c = 1; c < VISIBLE_W; c++) {
-        if((camera_x + c) < MAP_W) {
+        if((cam_x + c) < MAP_W) {
            
             if(tiles[t] != 0) {
                 QueueSpriteRect((c << TILE_ORD) - tile_scroll_x, 0, TILE_SIZE, TILE_SIZE - tile_scroll_y, tiles[t] << TILE_ORD, tile_scroll_y, 0);
@@ -73,7 +117,7 @@ void draw_world() {
     c = 0;
     r = 1;
     for(r = 1; r < VISIBLE_H; r++) {
-        if((camera_y + r) < MAP_H) {
+        if((cam_y + r) < MAP_H) {
             
             if(tiles[t] != 0) {
                 
@@ -81,7 +125,7 @@ void draw_world() {
             }
             t++;
             for(c = 1; c < VISIBLE_W; c++) {
-                if((camera_x + c) < MAP_W) {
+                if((cam_x + c) < MAP_W) {
                     if(tiles[t] != 0) {
                         QueueSpriteRect((c << TILE_ORD) - tile_scroll_x, (r << TILE_ORD) - tile_scroll_y, TILE_SIZE, TILE_SIZE, tiles[t] << TILE_ORD, 0, 0);
                     }
@@ -108,6 +152,7 @@ void main() {
     unsigned char anim_frame = 0;
     unsigned char anim_dir = 4;
     unsigned char anim_flip = 0;
+    unsigned int tx, ty;
     asm ("SEI");
 
     init_dynawave();
@@ -142,66 +187,46 @@ void main() {
         updateInputs();
 
         i = 0;
+        tx = player_x;
+        ty = player_y;
         if(inputs & INPUT_MASK_A) {
             if(anim_dir < 12) {
                 anim_dir += 12;
+                anim_frame = 0;
             }
             i = 1;
         } else {
+            if(anim_dir > 11) {
+                anim_dir -= 12;
+            }
             if(inputs & INPUT_MASK_RIGHT) {
                 anim_dir = 4;
                 anim_flip = 0;
                 i = 1;
-                tile_scroll_x++;
-                if(tile_scroll_x == TILE_SIZE) {
-                    tile_scroll_x = 0;
-                    camera_x++;
-                    if(camera_x == MAP_W-3) {
-                        camera_x = MAP_W-4;
-                        tile_scroll_x = TILE_SIZE-1;
-                    }
-                }
+                player_x++;
             } else if(inputs & INPUT_MASK_LEFT) {
                 anim_dir = 4;
                 anim_flip = SPRITE_FLIP_X;
                 i = 1;
-                tile_scroll_x--;
-                if(tile_scroll_x == 255) {
-                    tile_scroll_x = TILE_SIZE-1;
-                    camera_x--;
-                    if(camera_x == 255) {
-                        camera_x = 0;
-                        tile_scroll_x = 0;
-                    }
-                }
+                player_x--;
+            }
+            if(!character_tilemap_check(player_x, player_y)) {
+                player_x = tx;
             }
 
             if(inputs & INPUT_MASK_DOWN) {
                 anim_dir = 0;
                 anim_flip = 0;
                 i = 1;
-                tile_scroll_y++;
-                if(tile_scroll_y == TILE_SIZE) {
-                    tile_scroll_y = 0;
-                    camera_y++;
-                    if(camera_y == MAP_H - 3) {
-                        camera_y = MAP_H - 4;
-                        tile_scroll_y = TILE_SIZE-1;
-                    }
-                }
+                player_y++;
             } else if(inputs & INPUT_MASK_UP) {
                 anim_dir = 8;
                 anim_flip = 0;
                 i = 1;
-                tile_scroll_y--;
-                if(tile_scroll_y == 255) {
-                    tile_scroll_y = TILE_SIZE-1;
-                    camera_y--;
-                    if(camera_y == 255) {
-                        camera_y = 0;
-                        tile_scroll_y = 0;
-                    }
-                }
+                player_y--;
+            }
+            if(!character_tilemap_check(player_x, player_y)) {
+                player_y = ty;
             }
         }
 
@@ -222,7 +247,22 @@ void main() {
         draw_world();
         CLB(16);
         
-        QueuePackedSprite(&HeroMeta, 64, 64, walk_cycle[(anim_frame >> 3) & 3] + anim_dir, anim_flip, DMA_GRAM_PAGE);
+        camera_x = player_x - 64;
+        camera_y = player_y - 64;
+        if(camera_x & 0x8000) {
+            camera_x = 0;
+        }
+        if(camera_y & 0x8000) {
+            camera_y = 0;
+        }
+        if(camera_x > CAMERA_LIMIT) {
+            camera_x = CAMERA_LIMIT;
+        }
+        if(camera_y > CAMERA_LIMIT) {
+            camera_y = CAMERA_LIMIT;
+        }
+
+        QueuePackedSprite(&HeroMeta, player_x - camera_x, player_y - camera_y, walk_cycle[(anim_frame >> 3) & 3] + anim_dir, anim_flip, DMA_GRAM_PAGE);
         while(queue_pending != 0) {
             asm("CLI");
             wait();
