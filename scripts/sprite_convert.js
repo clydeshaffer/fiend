7e71aae1f4c0885940f7114b435526dd9f5ab928
Clyde Shaffer <clydeshaffer@gmail.com>
2022-07-02 10:28:02 -0400

    clean up sprite conversion script a bit

--- scripts/sprite_convert.js
+++ scripts/sprite_convert.js
@@ -3,37 +3,40 @@ const argv = require('minimist')(process.argv.slice(2));
 const BMP = require('bitmap-manipulation');
 
 const inFileName = argv._[0];
-const tmpFileName = inFileName + ".tmp";
 const outFileName = argv._.length == 2 ?
     argv._[1] :
     inFileName.split(".").slice(0, -1).join(".") + ".gtg";
 
 
-/*Jimp.read(inFileName)
-    .then( spriteSheet => {
-        spriteSheet.flip(false,true);
-        const w = spriteSheet.getWidth();
-        const h = spriteSheet.getHeight();
-        spriteSheet.getBuffer(Jimp.MIME_BMP).then(
-            gtgBuf => {
-                fs.writeFileSync(outFileName, gtgBuf.slice(-w * h));
-            }
-        );
-    });*/
+function flipV(inputImage) {
+    let flippedImage = new BMP.BMPBitmap(inputImage.width, inputImage.height, 1);
+    for(var i = 0; i < spriteSheet.getHeight(); i++) {
+        flippedImage.drawBitmap(inputImage, 0, i, null, 0, inputImage.height - i - 1, inputImage.width, inputImage.height);
+    }
+    return flippedImage;
+}
+
+const randchars = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNNM1234567890";
+function rndStr(len) {
+    var out = "";
+    for(var i = 0; i < len; i++) {
+        out += randchars[Math.floor(Math.random() * randchars.length)];
+    }
+    return out;
+}
+
+function saveHeadless(img, filename) {
+    var tmpname = rndStr(16) + ".tmp";
+    var pixelCount = img.width * img.height;
+    img.save(tmpname);
+    const tmpFileSize = fs.statSync(tmpname).size;
+    const outputStream = fs.createWriteStream(filename);
+    outputStream.on("finish", () => {
+        fs.unlinkSync(tmpname);
+    });
+    fs.createReadStream(tmpname, { start : (tmpFileSize - pixelCount)}).pipe(outputStream);
+}
 
 let spriteSheet = BMP.BMPBitmap.fromFile(inFileName);
-let outSheet = new BMP.BMPBitmap(spriteSheet.getWidth(), spriteSheet.getHeight(), 1);
-let pixelCount = spriteSheet.getHeight() * spriteSheet.getWidth();
-for(var i = 0; i < spriteSheet.getHeight(); i++) {
-    outSheet.drawBitmap(spriteSheet, 0, i, null, 0, spriteSheet.getHeight() - i - 1, spriteSheet.getWidth(), spriteSheet.getHeight());
-}
-outSheet.save(tmpFileName);
-
-const tmpFileSize = fs.statSync(tmpFileName).size;
-const outputStream = fs.createWriteStream(outFileName);
-
-outputStream.on("finish", () => {
-    fs.unlinkSync(tmpFileName);
-});
-
-fs.createReadStream(tmpFileName, { start : (tmpFileSize - pixelCount)}).pipe(outputStream);
+let outSheet = flipV(spriteSheet);
+saveHeadless(outSheet, outFileName);
